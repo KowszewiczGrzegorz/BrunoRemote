4619196dda01bf030d38fd09e5d0beadcadc4a06
timothy3001 <t.knufmann@gmail.com>
2014-12-17 18:26:36 +0100

    Test serial

--- index.js
+++ index.js
@@ -32,20 +32,14 @@ function registerTouchEvents() {
 
 function updateCam() {
 
-	 var xmlHTTP = new XMLHttpRequest();
+	var xmlHTTP = new XMLHttpRequest();
     xmlHTTP.open('GET',"/php/camPic.php?" + new Date().getTime(),true);
 
-    // Must include this line - specifies the response type we want
     xmlHTTP.responseType = 'arraybuffer';
     xmlHTTP.onload = function(e)
     {
         var arr = new Uint8Array(this.response);
-        // Convert the int array to a binary string
-        // We have to use apply() as we are converting an *array*
-        // and String.fromCharCode() takes one or more single values, not
-        // an array.
         var raw = String.fromCharCode.apply(null,arr);
-        // This works!!!
         var b64=btoa(raw);
         var dataURL="data:image/jpeg;base64,"+b64;
         document.getElementById("camPic").src = dataURL;
@@ -53,10 +47,14 @@ function updateCam() {
     };
 
     xmlHTTP.send();
-		
-	//document.getElementById("camPic").src = "/php/camPic.php?" + new Date().getTime();
 }
 
+
+function sendGetCall(address) {
+	xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", address, false );
+    xmlHttp.send( null );
+}
 
 
 /* ClickTouchHandlers Controls */
@@ -83,6 +81,7 @@ function clickTouchTurnRightPanel(e) {
 function clickTouchMoveForwardPanel(e) {
 	if (e.type == "touchstart" || e.type == "mousedown") {
 		document.getElementById("moveForwardPanel").style.background = "rgba(255,255,255,0.7)";
+		sendGetCall("php/moveForward.php");
 	}
 	else {
 		document.getElementById("moveForwardPanel").style.background = "";
@@ -97,9 +96,7 @@ function clickTouchMoveBackwardPanel(e) {
 	else {
 		document.getElementById("moveBackwardPanel").style.background = "";
 	}	
-
 }
-
 
 
 
--- php/moveForward.php
+++ php/moveForward.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" x-undefined="" />
-<title>Ohne_Titel_1</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+<?php
+	include "php_serial.class.php";
+	
+	$serial = new phpSerial;
+	$serial->deviceSet("/dev/ttyACM0");
+	$serial->confBaudRate(115200);
+	$serial->confParity("none");
+	$serial->confCharacterLength(8);
+	$serial->confStopBits(1);
+	$serial->deviceOpen();
+	$serial->sendMessage("PlaySound 1");	
+	$serial->deviceClose();
+?>
