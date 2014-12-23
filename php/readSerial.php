4fc8494bcf1b7bc2dac49127a2fc958a9ddb3728
timothy3001 <t.knufmann@gmail.com>
2014-12-23 17:34:25 +0100

    fix battery

--- php/readSerial.php
+++ php/readSerial.php
@@ -3,7 +3,6 @@
 	ini_set('display_errors', '1');
 	usleep(300000);
 	system('echo "' . $_GET["cmd"] .'\n"> /dev/ttyACM0');
-	usleep(300000);
-	$received = system('cat /dev/ttyACM0 & usleep(300000) ; kill $!');	
+	$received = system('cat /dev/ttyACM0 & sleep 1 ; kill $!');	
 	echo "Received: " . $received;
 ?>
