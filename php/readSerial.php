8bcf92382187655ce8253679ca76e73f80ea8e15
timothy3001 <t.knufmann@gmail.com>
2014-12-23 17:08:14 +0100

    debug

--- php/readSerial.php
+++ php/readSerial.php
@@ -3,7 +3,7 @@
 	ini_set('display_errors', '1');
 	
 	system('echo "' . $_GET["cmd"] .'\n"> /dev/ttyACM0');
-	usleep(300000);
+	usleep(1000000);
 	$received = system('cat /dev/ttyACM0 & usleep 300000 ; kill $!');	
 	echo "Received: " . $received;
 ?>
