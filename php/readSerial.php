11894a7435921b6bf7e3a5d98f631e912161862e
timothy3001 <t.knufmann@gmail.com>
2014-12-23 16:03:29 +0100

    battery

--- php/readSerial.php
+++ php/readSerial.php
@@ -3,6 +3,7 @@
 	ini_set('display_errors', '1');
 	
 	system('echo "' . $_GET["cmd"] .'\n"> /dev/ttyACM0');
+	sleep(1);
 	$received = system('cat /dev/ttyACM0 & sleep 1 ; kill $!');	
 	echo "Received: " . $received;
 ?>
